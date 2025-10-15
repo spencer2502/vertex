b4e46eda253f7934f6834888e6dffa2310d12a3e
[spencer] <amant19.nirmalkar@gmail.com>
2025-10-16 01:28:33 +0530

    Frontend Added

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useCallback, useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useCallback, useContext, useEffect } from "react";
+import { io,Socket } from "socket.io-client";
 
 interface SocketProviderProps {
   children?: React.ReactNode;
@@ -13,16 +13,39 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+
+
+export const useSocket = () => {
+    const state = useContext(SocketContext);
+    if (!state) {
+        throw new Error("useSocket must be used within a SocketProvider");
+    }
+
+    return state;
+}
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const [socket, setSocket] = React.useState<Socket>();
+
+
   const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
     console.log("Send message: ", msg);
-  }, []);
+
+    if(socket){
+        socket.emit("event:message" , {message: msg});
+    }
+
+  }, [socket]);
 
   useEffect(() => {
+
     const _socket = io("http://localhost:8000");
+    setSocket(_socket); 
+    
 
     return () => {
         _socket.disconnect();
+        setSocket(undefined);
     };
   }, []);
   return (
