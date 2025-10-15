4de8bb8d34bf64c879069dbe30e46af3e8e95d08
[spencer] <amant19.nirmalkar@gmail.com>
2025-10-16 00:41:13 +0530

    Socket Init

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,14 @@
 import http from "http";
+import SocketService from "./services/socket.js";
 
 async function init() {
+    const socketService = new SocketService();
+
     const httpServer = http.createServer();
 
     const PORT = process.env.PORT || 8000;
+
+    socketService.io.attach(httpServer)
 
     httpServer.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
